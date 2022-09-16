beaca06e6e2766aa5392d7213958a25cff244398
Oleg4779 <oleg.bernatsckij@yandex.ru>
2022-09-16 23:23:33 +0400

    First commit

---
+++ src/main/java/com/bernatskiy/springtestbot/SpringTestBotApplication.java
@@ -0,0 +1,13 @@
+package com.bernatskiy.springtestbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bernatskiy/springtestbot/SpringTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernatskiy.springtestbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
