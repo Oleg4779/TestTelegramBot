e3488742b62fce7e6ec6a5c2d15b23cd1dece938
Oleg4779 <oleg.bernatsckij@yandex.ru>
2022-09-25 00:29:36 +0300

    Changing to BerryStudio Bot

--- src/main/java/com/bernatskiy/springtestbot/BerryBot.java
+++ src/main/java/com/bernatskiy/springtestbot/BerryBot.java
@@ -1,14 +1,52 @@
 package com.bernatskiy.springtestbot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Optional;
 
 public class BerryBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            try {
+                handleMessage(update.getMessage());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void handleMessage(Message message) throws TelegramApiException {
+        //handle command
+        if (message.hasText() && message.hasEntities()) {
+            Optional<MessageEntity> commandEntity =
+                    message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
+            if (commandEntity.isPresent()) {
+                String command = message.getText().substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+
+                switch (command) {
+                    case "/set_currency":
+                        execute(SendMessage.builder()
+                                .text("Please, choose Original and Target Currencies")
+                                .chatId(message.getChatId().toString())
+                                .build());
+                        return;
+                }
+            }
+        }
+    }
+
     @Override
     public String getBotUsername() {
-        return "BerryStudioDemoBot";
+        return "SpringDemoBot";
     }
 
     @Override
@@ -16,24 +54,9 @@ public class BerryBot extends TelegramLongPollingBot {
         return "5746743263:AAGRhNqTDDpjzI7vKRvqzqdqmbzLe-8Nr_A";
     }
 
-    @Override
-    public void onUpdateReceived(Update update) {
-//        System.out.println(update.getMessage().getText());
-//        System.out.println(update.getMessage().getFrom().getFirstName());
-
-        String command = update.getMessage().getText();
-
-        if(command.equals("/run")) {
-            String message = "You entered message run";
-            SendMessage response = new SendMessage();
-            response.setChatId(update.getMessage().getChatId().toString());
-            response.setText(message);
-
-            try {
-                execute(response);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void main(String[] args) throws TelegramApiException {
+        BerryBot bot = new BerryBot();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
     }
 }
